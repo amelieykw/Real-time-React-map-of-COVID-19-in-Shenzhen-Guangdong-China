bb92ada2219887e65f94560e60b26ce23f515cad
amelieykw <yu.kaiwen.ykw@outlook.com>
2020-02-25 18:09:25 +0800

    feat: render google maps react without makers (#2)
    * feat:render google-maps-react without makers
    
    * docs:update README

--- frontend/App.js
+++ frontend/App.js
@@ -1,9 +1,10 @@
 import React from 'react';
+import GoogleApiWrapper from './components/googleMapsReact';
 
 function App() {
   return (
     <div>
-      <h1>Hello World</h1>
+      <GoogleApiWrapper />
     </div>
   );
 }
